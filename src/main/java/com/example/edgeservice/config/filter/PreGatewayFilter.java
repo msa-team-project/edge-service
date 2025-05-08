acc3320c35c6892f4e6ac7166960d09b03257c9c
rangrang-53 <jr0503@naver.com>
2025-05-08 17:57:33 +0900

    id check update

--- src/main/java/com/example/edgeservice/config/filter/PreGatewayFilter.java
+++ src/main/java/com/example/edgeservice/config/filter/PreGatewayFilter.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 
@@ -31,9 +32,11 @@ public class PreGatewayFilter extends AbstractGatewayFilterFactory<PreGatewayFil
         return (exchange, chain) -> {
             // 요청 경로를 확인하여 로그인 및 회원가입 요청이라면 토큰 체크를 우회
             String path = exchange.getRequest().getURI().getPath();
+            HttpMethod method = exchange.getRequest().getMethod();
+
             log.info("Request path: {}", path);
 
-            if (path.equals("/auths/login") || path.equals("/auths/join") || path.startsWith("/auths/email") || path.equals("/orders/prepare") || path.equals("/orders/update-fail") || path.equals("/orders/update-success")|| path.startsWith("/api/geocode") || path.equals("/auths/login/oauth") || path.equals("/auths/logout")) {
+            if (path.equals("/auths/login") || path.equals("/auths/join") || path.startsWith("/auths/email") || path.equals("/orders/prepare") || path.equals("/orders/update-fail") || path.equals("/orders/update-success")|| path.startsWith("/auths/check-id") || path.startsWith("/api/geocode") || path.equals("/auths/login/oauth") || path.equals("/auths/logout") || (method == HttpMethod.GET && path.startsWith("/reviews"))) {
                 // 로그인과 회원가입 요청에는 토큰 검증을 생략
                 return chain.filter(exchange);
             }
